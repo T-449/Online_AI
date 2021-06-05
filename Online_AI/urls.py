1e7d7017f2700a59d3dd41703777d692a5285240
hsiam261 <hsiam261@gmail.com>
2021-06-05 11:14:52 +0600

    game model

--- Online_AI/urls.py
+++ Online_AI/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+## media root thingy
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('test/', include('test.urls')),
     path('workspace/', include('game_creator.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
