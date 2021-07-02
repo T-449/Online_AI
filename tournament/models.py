1d29a21d9eaf67f7c7818fa2700eb665fee136d2
Tushin Mallick <purgatory@Tartarus.local>
2021-07-02 21:27:01 +0600

    Updated tournament and added urls

--- tournament/models.py
+++ tournament/models.py
@@ -1,3 +1,4 @@
+import django.contrib.auth.models
 from django.db import models
 from game_creator.models import Game
 
@@ -11,3 +12,37 @@ class TournamentInfo(models.Model):
     startTime = models.DateTimeField(null=True)
     endTime = models.DateTimeField(null=True)
     phase = models.CharField(max_length=20, null=False, default="Registration")
+
+
+class TournamentCreatorWorkspaceACL(models.Model):
+    user = models.ForeignKey(django.contrib.auth.models.User, on_delete=models.CASCADE)
+    tournament = models.ForeignKey(TournamentInfo, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'tournament',)
+
+
+def tournament_creator_validate_workspace_access(user, tournament):
+    try:
+        TournamentCreatorWorkspaceACL.objects.get(user=user, tournament=tournament)
+        return True
+    except (KeyError, TournamentCreatorWorkspaceACL.DoesNotExist):
+        return False
+    return False
+
+
+class TournamentParticipantWorkspaceACL(models.Model):
+    user = models.ForeignKey(django.contrib.auth.models.User, on_delete=models.CASCADE)
+    tournament = models.ForeignKey(TournamentInfo, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'tournament',)
+
+
+def tournament_participant_validate_workspace_access(user, tournament):
+    try:
+        TournamentParticipantWorkspaceACL.objects.get(user=user, tournament=tournament)
+        return True
+    except (KeyError, TournamentParticipantWorkspaceACL.DoesNotExist):
+        return False
+    return False
--- tournament/urls.py
+++ tournament/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.createTournament, name='createTournament'),
     path('tournamentCreation', views.tournamentCreation, name='tournamentCreation'),
-    path('basetab', views.baseTab, name='baseTab'),
+    path('<int:tournament_id>basetab', views.baseTab, name='baseTab'),
+    path('reg_unreg', views.reg_unreg, name='reg_unreg'),
+    path('tournamentList', views.tournamentList, name='tournamentList'),
 ]
