3e6c8d5c1cd5f33317bd25cb7866172673f43188
hsiam261 <hsiam261@gmail.com>
2021-07-02 21:57:41 +0600

    Hoping this fixes deleting migrations :3

--- tournament/admin.py
+++ tournament/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from tournament.models import TournamentInfo
+from tournament.models import TournamentCreatorWorkspaceACL
 
 admin.site.register(TournamentInfo)
+admin.site.register(TournamentCreatorWorkspaceACL)
