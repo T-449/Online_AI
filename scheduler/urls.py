a7f89cdf61d8ac3e0cd0526647972f53fd04929e
anachor <anachored@gmail.com>
2021-07-21 05:16:43 +0600

    Clean shit up

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -2,5 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('showjobs', views.show_jobs, name='show_jobs'),
 ]
--- scheduler/views.py
+++ scheduler/views.py
@@ -1,15 +1,2 @@
 from django.shortcuts import render
 
-# Create your views here.
-
-def show_jobs(request):
-    if (request.user.is_superuser):
-        jobs = getScheduledJobs()
-        context = {
-            'jobs': jobs
-        }
-        print(jobs)
-        return render(request, 'scheduler/showjobs.html', context)
-
-    else:
-        raise Http404
